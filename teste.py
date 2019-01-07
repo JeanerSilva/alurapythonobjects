4673ced5f9b44400fba7ab10d5ba46098e841762
JeanerSilva <jeanerufmg@gmail.com>
2019-01-07 21:27:38 -0200

    esptool

--- teste.py
+++ teste.py
@@ -1,12 +1,23 @@
+from Conta import Conta
+
+
 def cria_conta(numero, titular, saldo, limite):
     conta = {"numero": numero, "titular": titular, "saldo": saldo, "limite": limite}
     return conta
 
+
 def deposita(conta, valor):
     conta["saldo"] += valor
+
 
 def saca(conta, valor):
     conta["saldo"] -= valor
 
+
 def extrato(conta):
     print("Saldo {}".format(conta["saldo"]))
+
+
+conta1 = Conta(123, "Jeaner", 100, 1000)
+conta1.saca(20)
+print (conta1.saldo)
